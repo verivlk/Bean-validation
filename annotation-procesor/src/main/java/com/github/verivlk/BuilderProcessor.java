717f3d86c076d92cc274b2a4e80e1bdf1a6e9b73
Veri <verivlk@gmail.com>
2021-01-14 11:28:24 +0100

    created modules and procesor class

---
+++ annotation-procesor/src/main/java/com/github/verivlk/BuilderProcessor.java
@@ -0,0 +1,31 @@
+package com.github.verivlk;
+
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes(
+        "javax.validation.Valid")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
+public class BuilderProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations,
+                           RoundEnvironment roundEnv) {
+
+        for (TypeElement annotation : annotations) {
+            Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
+            System.out.println("anotace");
+            System.out.println(annotation.toString());
+            // …
+        }
+
+        return true;
+    }
+}
